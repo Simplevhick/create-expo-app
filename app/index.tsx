3ea4012fddd588a2dd27a9b239ceb0cc3b3f8ad6
chimbor victor <chimborvictor10@gmail.com>
2024-11-11 19:32:32 +0100

    install gluestack

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
 import { View, Text, FlatList } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 
 export default function HomeScreen() {
-  return (
-    <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item} />} />
-  );
+    return (
+        <Button variant="outline">
+            <ButtonText>Press me</ButtonText>
+        </Button>
+    )
+//   return (
+//     <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item} />} />
+//   );
+
 }
