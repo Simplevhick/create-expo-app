bb0410e1489645e80955f3f1aa52cd4d77e964af
chimbor victor <chimborvictor10@gmail.com>
2024-11-11 17:57:02 +0100

    setup expo router

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello people!</Text>
+      <Text>Hello peoples!</Text>
       <StatusBar style="auto" />
     </View>
   );
